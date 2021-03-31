32b76e2fad2f96ff78811214b770356974790597
obareau <olivier.bareau@gmail.com>
2021-03-31 08:31:46 +0200

    Add files via upload

---
+++ Integer_or_float.py
@@ -0,0 +1,24 @@
+# integer or float 
+
+x_position = 10
+print(x_position)
+# integer
+x_position = 15
+print(x_position)
+
+print(type(x_position))
+# float
+x_position = 15.0
+print(x_position)
+
+print(type(x_position))
+# attention au virgule !!!!
+x_position = 15,0
+print(x_position)
+
+print(type(x_position))
+# string
+x_position = "abc"
+print(x_position)
+
+print(type(x_position))
---
+++ bool.py
@@ -0,0 +1,20 @@
+is_game_over = False
+is_game_over = True
+
+print(is_game_over)
+#On a bien un type booléen !
+print(type(is_game_over))
+
+is_game_over = 1
+print(is_game_over)
+# on assigne 1 donc on chage le type en Integer
+print(type(is_game_over))
+
+name = "Olivier"
+
+I_age = 49
+# {} est  un placeholder 
+S_name_and_age = "Olivier: {}".format(I_age)
+print (S_name_and_age)
+# On a bien une String si on vérifie
+print(type(S_name_and_age))
