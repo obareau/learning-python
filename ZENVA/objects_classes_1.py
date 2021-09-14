6d531b53668f230a4f641a3a70bcce11f8dce9a0
Olivier Bareau <olivier.bareau@gmail.com>
2021-09-14 19:08:13 +0200

    objects & classes part 1

---
+++ ZENVA/objects_classes_1.py
@@ -0,0 +1,14 @@
+class GameObject:
+    # blueprint est défini ici
+    def __init__(self, name, x_pos, y_pos) :
+        self.name = name
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+    # Fin idu blueprint
+    
+game_object = GameObject ("Enemy", 1, 2)
+# On accéde à la variable name de l'objet game_objet
+print (game_object.name)
+# On réassigne name dans le game_object
+game_object.name = "Ennemy 1"
+print (game_object.name)
