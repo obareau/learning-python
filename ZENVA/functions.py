0960f797498b78cdbe8309ec8cd0fd9906f09c58
Olivier Bareau <olivier.bareau@gmail.com>
2021-09-14 16:51:10 +0200

    première version du code

---
+++ ZENVA/functions.py
@@ -0,0 +1,22 @@
+# Les fonctions sont un bon mye, de répéter du code
+# Ca permet aussi de controller où et quand notre code
+# S'éxécute
+# Les fonces fonctions acceptent des valeurs en entrée
+# Et on peut récuprer leurs sorties sous forme de résultat
+# UNE FONCTION - UN BUT ET UN SEUL
+# On n simplifie et si c'est complexe on split en fonctions plus petites
+
+position = 0
+
+def move_player():
+    global position
+    position += 1
+    print(position)
+
+# Rien ne se passe tant que j'apelle pas ma fonction
+move_player()
+# Et je oeux l'appeler autant de fois que je veux (Captain Obvious !!!)
+move_player()
+move_player()
+move_player()
+move_player()
