4f044792125b887dd07405d81057d7448a293bdc
Olivier Bareau <olivier.bareau@gmail.com>
2021-09-19 19:55:57 +0200

    on crée la fenêtre de jeu

--- Pygame_Rpg/Crossy_Rpg_Game/main.py
+++ Pygame_Rpg/Crossy_Rpg_Game/main.py
@@ -3,6 +3,14 @@ import pygame
 pygame.init()
 
 # Game code
+# on crée la fenétre de jeu
+# C'est mieux de définir notre tuple pour la taille grâce à des variables
+width = 800
+height = 800
+white_colour = (255,255,255) # value en rgb
+game_window = pygame.display.set_mode((width,height))
+game_window.fill((white_colour))
+pygame.display.update()  
 
 pygame.quit()
 quit()
