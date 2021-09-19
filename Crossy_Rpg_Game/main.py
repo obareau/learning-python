985bede09c84889b23e48c834d87d0910cac40a1
Olivier Bareau <olivier.bareau@gmail.com>
2021-09-19 23:06:24 +0200

    on rajoute le trésor

--- Crossy_Rpg_Game/main.py
+++ Crossy_Rpg_Game/main.py
@@ -19,6 +19,9 @@ background_image = pygame.image.load("assets/background.png")
 # pb de chemin /Users/macuser/Documents/PYTHON-OLIVIER/learning-python/Pygame_Rpg/Crossy_Rpg_Game/main.py
 # ne fonctionne pas dans le shell avec Pygame_Rpg/Crossy_Rpg_Game/assets/background.png
 background = pygame.transform.scale(background_image,(width,height))
+
+treasure_image = pygame.image.load("assets/treasure.png")
+treasure = pygame.transform.scale(treasure_image, (50, 50))
 # Game loop
 def run_game_loop(): # avec une fonction on break tout le flow !
     while True:
@@ -27,6 +30,8 @@ def run_game_loop(): # avec une fonction on break tout le flow !
         # on affiche l'image de fond grace à la fonction blit
         game_window.blit(background, (0,0)) # le tuple (x,y) = (0,0)= top left corner
         # on blit avant de tout rafraichir
+        # on affiche le trésor
+        game_window.blit(treasure; (375, 50))
         pygame.display.update()  
         
         # Handle events
