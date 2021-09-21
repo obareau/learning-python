e4c8821ed7974c616568f2c5db80df406f9ac279
Olivier Bareau <olivier.bareau@gmail.com>
2021-09-21 16:38:01 +0200

    clean source

--- Crossy_Rpg_Game/game.py
+++ Crossy_Rpg_Game/game.py
@@ -6,9 +6,7 @@ from gameObject import GameObject
 class Game:
     
     def __init__(self):
-        # C'est mieux de définir notre tuple pour la taille grâce à des variables
-        # Plutot que de hardcoder comme un porc
-        # avec .self ca devient une propriété propre à un objet de la classe Game
+        
         self.width = 800
         self.height = 800
         self.white_colour = (255,255,255) # value en rgb
@@ -18,14 +16,9 @@ class Game:
         self.clock = pygame.time.Clock() # on crée une instance de Clock du module pygame.time
         
         self.background = GameObject(0,0, self.width, self.height, "Crossy_Rpg_Game/assets/background.png")
-        # background_image = pygame.image.load("Crossy_Rpg_Game/assets/background.png") 
-        # # pb de chemin /Users/macuser/Documents/PYTHON-OLIVIER/learning-python/Pygame_Rpg/Crossy_Rpg_Game/main.py
-        # # ne fonctionne pas dans le shell avec Pygame_Rpg/Crossy_Rpg_Game/assets/background.png
-        # self.background = pygame.transform.scale(background_image,(self.width,self.height))
-        self.treasure = GameObject(375, 50, self.width, self.height, "Crossy_Rpg_Game/assets/treasure.png")
-        treasure_image = pygame.image.load("Crossy_Rpg_Game/assets/treasure.png")
         
-        self.treasure = pygame.transform.scale(treasure_image, (50, 50))
+        self.treasure = GameObject(375, 50, 50,50 , "Crossy_Rpg_Game/assets/treasure.png")
+        
         
     def draw_objects(self):
         # on veut remplir la window de blanc tant que le joeur quitte pas
