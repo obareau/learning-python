2694811e354fda40c4ba128531403288a51d787d
Olivier Bareau <olivier.bareau@gmail.com>
2021-09-21 16:00:34 +0200

    ajout de la virgule après image ligne 8

--- Crossy_Rpg_Game/gameObject.py
+++ Crossy_Rpg_Game/gameObject.py
@@ -5,7 +5,7 @@ class GameObject:
     
     def __init__(self, x, y, width, height, image_path):
         image = pygame.image.load(image_path) # on crée une var locale
-        self.image = pygame.transform.scale(image (width,height )) 
+        self.image = pygame.transform.scale(image, (width,height )) 
         
         self.x = x
         self.y = y
